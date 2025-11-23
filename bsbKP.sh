0519b52e2de103ac9f428679aa7965fa5eab0654
port dir <portdirfile@outlook.com>
2025-11-23 15:33:13 -0800

    Add files via upload

--- bsbKP.sh
+++ bsbKP.sh
@@ -1,13 +1,12 @@
 # by portdir 11/19/25
-# V 1.5
+# V 1.6
 # BSB updater made for 6.17.7 arch
 
 cd ~/Desktop/
 # get tool
-
 sudo pacman -S devtools base-devel pacman-contrib
 
-#fix usb stuff i think
+# fix usb stuff i think
 sudo install -m 0644 /dev/stdin /etc/udev/rules.d/71-bigscreen-beyond.rules << 'EOF'
 # Bigscreen Beyond access for seated user
 SUBSYSTEM=="usb", ATTR{idVendor}=="35bd", TAG+="uaccess"
@@ -27,7 +26,7 @@ lsusb -d 35bd:4004 || true
 # sudo nano /etc/makepkg.conf
 # MAKEFLAGS="-j$(nproc)"
 #
-# this file is to big for this might need to look in to it
+# this build file is to big for this might need to look in to it
 # BUILDDIR=/tmp/makepkg
 # ++++++++++++++++++++++++++++++++
 
@@ -52,18 +51,40 @@ time makepkg -s --skippgpcheck
 # add to pacman
 sudo pacman -U BSBvrARCH-*.pkg.tar.zst
 
-# add to grub+
+# add to boot lodder (WIP)
+
+# grub
 #sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# systemd-boot
+# /boot/loader/entries/
+#echo "title   BSB Arch Linux (linux)
+#linux   /vmlinuz-BSBvrARCH
+#initrd  /initramfs-BSBvrARCH.img
+#options root=PARTUUID=?????????????????? rw rootfstype=????" >> /boot/loader/entries/BSBvrARCH.conf
 
 # clean up
 rm -rf ~/Desktop/buildingthatvrlinuxthing
 
-# vr add ons
+#===============================================================================================
+
+# VR ADD ONS
 
 #mkdir ~VRthings/
-
-# wlx
+# wlx overlay
 #curl -O https://github.com/galister/wlx-overlay-s/releases/download/v25.4.2/WlxOverlay-S-v25.4.2-x86_64.AppImage
-
 #mv WlxOverlay-S-v25.4.2-x86_64.AppImage ~/VRthings/
 #chmod +x ~VRthings/WlxOverlay-S-v25.4.2-x86_64.AppImage
+
+# envision
+#yay -S envision-xr-git
+#sudo pacman -Syu wayland-protocols
+#yay -S libudev0-shim
+
+# vr  stuff
+#yay -S opencomposite-git xrizer-git
+
+# protonup-qt
+#yay -S protonup-qt
+
+#===============================================================================================
